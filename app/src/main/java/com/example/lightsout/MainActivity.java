7056f72e63792f749dd85787ea9a3bc15976d192
Tommy <naviboun@gmail.com>
2025-09-09 12:07:48 -0500

    Added the method to change the button color.

--- app/src/main/java/com/example/lightsout/MainActivity.java
+++ app/src/main/java/com/example/lightsout/MainActivity.java
@@ -13,11 +13,13 @@ public class MainActivity extends AppCompatActivity {
 
     public static final int GRID_SIZE = 3;
     private GridLayout grid;
-    private boolean cellState [][];
+    private boolean cellState[][];
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
 
         cellState = new boolean[][]{{true, true, true}, {true, true, true}, {true, true, true}};
 
@@ -27,9 +29,17 @@ public class MainActivity extends AppCompatActivity {
 //      randomize();
 
         recolor();
+
+        for (int i = 0; i < grid.getChildCount(); i++) {
+
+            Button currButton = (Button) grid.getChildAt(i);
+            currButton.setOnClickListener(buttonListener);
+        }
+
     }
 
-    public void recolor(){
+
+    public void recolor() {
         for (int i = 0; i < grid.getChildCount(); i++) {
             Button gridButton = (Button) grid.getChildAt(i);
 
@@ -38,22 +48,46 @@ public class MainActivity extends AppCompatActivity {
             int col = i % GRID_SIZE;
 
             if (cellState[row][col] == true) {
-                gridButton.setBackgroundColor(getColor(R.color.blue_500));
+                gridButton.setBackgroundColor(getColor(R.color.aquamarine));
             } else {
-                gridButton.setBackgroundColor(getColor(R.color.black));
+                gridButton.setBackgroundColor(getColor(R.color.mint));
             }
         }
     }
 
-    public void randomize(){
+    public void randomize() {
         Random random = new Random();
-        for(int i =0; i< GRID_SIZE; i++){
-            for(int j =0; j< GRID_SIZE; j++){
+        for (int i = 0; i < GRID_SIZE; i++) {
+            for (int j = 0; j < GRID_SIZE; j++) {
                 cellState[i][j] = random.nextBoolean();
             }
         }
     }
 
+    View.OnClickListener buttonListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Button current = (Button) view;
 
 
+            for (int i = 0; i < grid.getChildCount(); i++) {
+                Button gridButton = (Button) grid.getChildAt(i);
+
+                if (gridButton == current) {
+                    int row = i / GRID_SIZE;
+                    int col = i % GRID_SIZE;
+
+                    if (cellState[row][col] == true) {
+                        cellState[row][col] = false;
+                    } else {
+                        cellState[row][col] = true;
+                    }
+                }
+            }
+            recolor();
+        }
+
+
+
+    };
 }
